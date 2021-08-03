81b04f4b20d2f5b52d2d98f2a4c868e2c8a38ba8
Luke Goyer <goyerl@gmail.com>
2021-08-02 18:26:49 -0700

    cleanup

--- exporter.py
+++ exporter.py
@@ -1,24 +1,25 @@
 import logging
 import os
 import time
-import dateutil.parser as dp
 
 from datetime import datetime, timedelta
 from srpenergy.client import SrpEnergyClient
 from prometheus_client import start_http_server
-from prometheus_client import Gauge, Histogram
 from prometheus_client.core import (
-    GaugeMetricFamily, Timestamp, REGISTRY
+    GaugeMetricFamily, REGISTRY
 )
 
 logging.getLogger().setLevel(logging.INFO)
-accountid = os.environ['SRP_ID']
-username = os.environ['SRP_USER']
-password = os.environ['SRP_PASS']
+accountid = os.environ["SRP_ID"]
+username = os.environ["SRP_USER"]
+password = os.environ["SRP_PASS"]
 client = SrpEnergyClient(accountid, username, password)
 
 
 class CustomCollector(object):
+    """
+    Collect yesterday's metrics from SrpEnergyClient 
+    """
     def __init__(self):
         pass
 
@@ -31,7 +32,7 @@ class CustomCollector(object):
 
     def collect(self):
         logging.info("collection_running")
-        usage_gauge = GaugeMetricFamily("energy_usage", 'Help text', labels=["account"])
+        usage_gauge = GaugeMetricFamily("energy_usage", "Help text", labels=["account"])
         cost_gauge = GaugeMetricFamily("energy_cost", "Help Text", labels=["account"])
         metrics = self.get_yesterday()
         total_usage = 0
@@ -41,7 +42,7 @@ class CustomCollector(object):
             date, hour, isodate, kwh, cost = i
             total_cost += cost
             total_usage += kwh
-            
+
         usage_gauge.add_metric([accountid], total_usage)
         cost_gauge.add_metric([accountid], total_cost)
         yield cost_gauge
@@ -49,7 +50,7 @@ class CustomCollector(object):
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_http_server(8000)
     REGISTRY.register(CustomCollector())
     while True:
